dd19976e5b7c787a5cfdb1fad62685dab472634f
walker-finlay <walker.finlay@gmail.com>
2022-04-19 19:54:11 -0400

    columns height set by parent

--- src/pages/faq/index.js
+++ src/pages/faq/index.js
@@ -1,7 +1,8 @@
 import * as React from "react";
 import Helmet from "react-helmet";
 import FAQJSON from "../../../content/faq.json";
-import SingleFAQ from "./components/singleFAQ";
+import Columns from "../../components/columns/Columns";
+import SingleFAQ from "./components/SingleFAQ";
 
 import "./index.scss";
 
@@ -11,6 +12,7 @@ const FAQPage = () => (
       <meta charSet="utf-8" />
       <title>FAQ | GABAGHOULS NFT</title>
     </Helmet>
+    <Columns />
     <h1>FAQ</h1>
     <h2 className="drop-tip">Click a question to see the answer</h2>
     {FAQJSON.content.map((item, idx) => (
--- src/pages/roadmap/index.js
+++ src/pages/roadmap/index.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Helmet from "react-helmet";
 import RoadMapSection from "./components/RoadMapSection";
+import Columns from "../../components/columns/Columns";
 
 import roadMapJSON from "../../../content/roadmap.yaml";
 
@@ -12,6 +13,7 @@ const RoadMapPage = () => (
       <meta charSet="utf-8" />
       <title>ROADMAP | GABAGHOULS NFT</title>
     </Helmet>
+    <Columns />
     <h1>Roadmap</h1>
     <h2 className="drop-tip">Click an entry to see more</h2>
     <div className="roadmap-sections">
