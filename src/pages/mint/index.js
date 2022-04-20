96e004b1d2ebb56ee55b358bacbcfd0fff3d07ee
walker-finlay <walker.finlay@gmail.com>
2022-04-19 20:09:59 -0400

    mint coming soon

--- src/pages/mint/index.js
+++ src/pages/mint/index.js
@@ -1,5 +1,8 @@
 import * as React from "react";
 import Helmet from "react-helmet";
+import Columns from "../../components/columns/Columns";
+
+import "./index.scss";
 
 const MintPage = () => (
   <main className="mint">
@@ -7,7 +10,14 @@ const MintPage = () => (
       <meta charSet="utf-u" />
       <title>MINT | GABAGHOULS NFT</title>
     </Helmet>
+    <Columns />
     <h1>Mint</h1>
+    <div className="coming-soon">
+      <div>
+        <h2>Coming Soon!</h2>
+        <p>To get early access, head over to our socials to enter one of the 1/1 giveaways!</p>
+      </div>
+    </div>
   </main>
 );
 
