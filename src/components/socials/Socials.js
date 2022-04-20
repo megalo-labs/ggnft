ad53efff4ecb2cb671ba17d6d57671979fea0af0
walker-finlay <walker.finlay@gmail.com>
2022-04-20 11:13:47 -0400

    open sea link

--- src/components/socials/Socials.js
+++ src/components/socials/Socials.js
@@ -5,24 +5,28 @@ import Instagram from "../../../assets/instagram.svg";
 import TikTok from "../../../assets/tiktok.svg";
 import Twitter from "../../../assets/twitter.svg";
 import YouTube from "../../../assets/youtube.svg";
+import OpenSea from "../../../assets/opensea.svg";
 
 import "./Socials.scss";
 
 const Socials = () => (
   <div className="social-icons">
-    <a href="https://discord.gg/2YWCpXF6GX" target="_blank" rel="noreferrer">
+    <a href="/" title="OpenSea">
+      <OpenSea />
+    </a>
+    <a href="https://discord.gg/2YWCpXF6GX" target="_blank" rel="noreferrer" title="Discord">
       <Discord />
     </a>
-    <a href="https://www.instagram.com/gabaghoulsnft/" target="_blank" rel="noreferrer">
+    <a href="https://www.instagram.com/gabaghoulsnft/" target="_blank" rel="noreferrer" title="Instagram">
       <Instagram />
     </a>
-    <a href="https://www.tiktok.com/@gabaghoulsnft" target="_blank" rel="noreferrer">
+    <a href="https://www.tiktok.com/@gabaghoulsnft" target="_blank" rel="noreferrer" title="TikTok">
       <TikTok />
     </a>
-    <a href="https://www.youtube.com/channel/UC3WuYZ33gpSRShdptlAwfLg" target="_blank" rel="noreferrer">
+    <a href="https://www.youtube.com/channel/UC3WuYZ33gpSRShdptlAwfLg" target="_blank" rel="noreferrer" title="YouTube">
       <YouTube />
     </a>
-    <a href="https://mobile.twitter.com/gabaghoulsnft" target="_blank" rel="noreferrer">
+    <a href="https://mobile.twitter.com/gabaghoulsnft" target="_blank" rel="noreferrer" title="Twitter">
       <Twitter />
     </a>
   </div>
